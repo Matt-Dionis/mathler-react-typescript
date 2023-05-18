0ac8ad8ffe7d9ba8fab1461be1d8612a56a3a610
Dionii <mattdionis@Matts-MacBook-Pro.local>
2023-05-18 10:18:24 -0400

    Add tests to useGame hook

--- src/hooks/useGame.test.ts
+++ src/hooks/useGame.test.ts
@@ -1,40 +1,45 @@
 import '@testing-library/jest-dom'
-import { renderHook } from '@testing-library/react'
+import { act, renderHook } from '@testing-library/react'
 import useGame from './useGame'
 
 describe('useGame hook', () => {
-  it('has default data', () => {
-    const { result: { current } } = renderHook(() => useGame())
-    const [
-      {
-        currentColumnIndex,
-        currentRowIndex,
-        disabledKeys,
-        exactMatches,
-        grid,
-        looseMatches,
-        solution,
-        status,
-        total
-      },
-      {
-        deleteLatestEntry,
-        handleValueOrOperatorClick,
-        submitSolutionAttempt
-      }
-    ] = current
-    expect(currentColumnIndex).toEqual(0)
-    expect(currentRowIndex).toEqual(0)
-    expect(disabledKeys?.length).toEqual(0)
-    expect(exactMatches?.length).toEqual(0)
-    expect(grid?.length).toEqual(6)
-    expect(looseMatches?.length).toEqual(0)
-    expect(solution).toEqual(['7', '*', '8', '-', '1', '2'])
-    expect(status?.complete).toEqual(false)
-    expect(status?.success).toEqual(false)
-    expect(total).toEqual(44)
-    expect(deleteLatestEntry).toBeDefined()
-    expect(handleValueOrOperatorClick).toBeDefined()
-    expect(submitSolutionAttempt).toBeDefined()
+  const { result } = renderHook(() => useGame())
+
+  it('has default data and mutations are properly handled', () => {
+    expect(result.current[0].currentColumnIndex).toEqual(0)
+    expect(result.current[0].currentRowIndex).toEqual(0)
+    expect(result.current[0].disabledKeys?.length).toEqual(0)
+    expect(result.current[0].exactMatches?.length).toEqual(0)
+    expect(result.current[0].grid?.length).toEqual(6)
+    expect(result.current[0].looseMatches?.length).toEqual(0)
+    expect(result.current[0].solution).toEqual(['7', '*', '8', '-', '1', '2'])
+    expect(result.current[0].status?.complete).toEqual(false)
+    expect(result.current[0].status?.success).toEqual(false)
+    expect(result.current[0].total).toEqual(44)
+    expect(result.current[1].deleteLatestEntry).toBeDefined()
+    expect(result.current[1].handleValueOrOperatorClick).toBeDefined()
+    expect(result.current[1].submitSolutionAttempt).toBeDefined()
+
+    expect(result.current[0].grid[0][0]).toBe('')
+
+    void act(() => { result.current[1].handleValueOrOperatorClick('7') })
+    void act(() => { result.current[1].handleValueOrOperatorClick('*') })
+    void act(() => { result.current[1].handleValueOrOperatorClick('8') })
+    void act(() => { result.current[1].handleValueOrOperatorClick('-') })
+    void act(() => { result.current[1].handleValueOrOperatorClick('1') })
+    void act(() => { result.current[1].handleValueOrOperatorClick('1') })
+    expect(result.current[0].grid[0][5]).toBe('1')
+    jest.spyOn(window, 'alert').mockImplementation(() => {})
+    void act(() => { result.current[1].submitSolutionAttempt() })
+    expect(window.alert).toBeCalledWith(`Every guess must equal ${result.current[0].total}`)
+    expect(result.current[0].status.complete).toBe(false)
+    expect(result.current[0].status.success).toBe(false)
+
+    void act(() => { result.current[1].deleteLatestEntry() })
+    expect(result.current[0].grid[0][5]).toBe('')
+    void act(() => { result.current[1].handleValueOrOperatorClick('2') })
+    void act(() => { result.current[1].submitSolutionAttempt() })
+    expect(result.current[0].status.complete).toBe(true)
+    expect(result.current[0].status.success).toBe(true)
   })
 })
